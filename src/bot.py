81316c8bb0590d9bb15e685107a591f6ff7d6a01
OmarLucero29 <omarlucero.itm@gmail.com>
2025-04-20 12:59:08 -0600

    Update bot.py

--- src/bot.py
+++ src/bot.py
@@ -1,1 +1,14 @@
+from telegram import Update  
+from telegram.ext import Updater, CommandHandler, CallbackContext  
 
+def start(update: Update, context: CallbackContext):  
+    update.message.reply_text("¡Hola! Soy tu bot de apuestas. Usa /tips.")  
+
+def main():  
+    updater = Updater("TU_TOKEN_AQUÍ", use_context=True)  
+    updater.dispatcher.add_handler(CommandHandler('start', start))  
+    updater.start_polling()  
+    updater.idle()  
+
+if __name__ == "__main__":  
+    main()  
